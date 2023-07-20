36914d1cea43f6bacbc0450da3427cfd7e53f1f0
Piotr Wlazłowski <wlazlowskipiotr@gmail.com>
2023-07-20 12:52:41 +0200

    Calculator feature #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println(Calculator.add(2,6));
+        System.out.println(Calculator.subtract(80,24));
+        System.out.println(Calculator.multiply(5,5));
+        System.out.println(Calculator.divide(40,5));
     }
 }
